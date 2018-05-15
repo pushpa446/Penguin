9f15c1cf53de2a30540940a7d1b5debac5075e2b
Shirisha Kodimala <kshiri@thoughtworks.com>
2018-05-15 15:33:25 +0530

    Shirisha  | Added REST api call

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+var https = require('http');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+// app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+var optionsget = {
+    host : '127.0.0.1',
+    port : 3001,
+    path : '/users',
+    method : 'GET'
+};
+
+var reqGet = https.request(optionsget, function(res) {
+    console.log("statusCode: ", res.statusCode);
+    res.on('data', function(d) {
+        console.info('GET result:\n');
+        process.stdout.write(d);
+        console.info('\n\nCall completed');
+    });
+
+});
+
+reqGet.end();
+reqGet.on('error', function(e) {
+    console.error(e);
+});
